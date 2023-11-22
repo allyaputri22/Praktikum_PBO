7d77bd2d7f6dea596cc5842096b76e9bec013841
allyaputri22 <114640843+allyaputri22@users.noreply.github.com>
2023-11-22 18:14:26 +0700

    Add files via upload

---
+++ tuprak6/no1/tuprak6/HurufZtoA.java
@@ -0,0 +1,10 @@
+package no1.tuprak6;
+
+public class HurufZtoA {
+    public static void main(String[] args) {
+        char huruf;
+        for (huruf = 'Z'; huruf >= 'A'; huruf--) {
+            System.out.print(huruf + " ");
+        }
+    }
+}
